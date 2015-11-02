51a6a26accac92d9242b7e5c04ecb1ce3b617d85
ambarket <ambarket@gmail.com>
2015-11-02 01:33:57 -0500

    Fixed compile script

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
-javac -cp src src/*.java
+javac -cp src/plotStuff.jar src/*.java src/regressionTree/*.java src/gbm/*.java src/gbm/cv/*.java src/dataset/*.java src/utilities/*.java
 
-java -cp src/plotStuff.jar:src/ Main
+java -cp src/plotStuff.jar:src/:src/regressionTree:src/gbm:src/gbm/cv:src/dataset:src/utilities Main
 
