db656de6af4df22f532acc5190889813c83725dc
ambarket <ambarket@gmail.com>
2015-11-11 08:22:17 -0500

    Small fix

--- src/dataset/DatasetParameters.java
+++ src/dataset/DatasetParameters.java
@@ -13,7 +13,7 @@ public class DatasetParameters {
 	public DatasetParameters(String minimalName, String fullName, String fileDirectory, String fileName, int responseVariableColumn, boolean attributeTypeHeader, boolean attributeNameHeader) {
 		this.minimalName = minimalName;
 		this.fullName = fullName;
-		this.fileDirectory = (fileDirectory.contains(System.getProperty("user.dir"))) ? (System.getProperty("user.dir") + fileDirectory) : fileDirectory;
+		this.fileDirectory = (!fileDirectory.contains(System.getProperty("user.dir"))) ? (System.getProperty("user.dir") + "/"+ fileDirectory) : fileDirectory;
 		this.fileName = fileName;
 		this.responseVariableColumn = responseVariableColumn;
 		this.attributeNameHeader = attributeNameHeader;
