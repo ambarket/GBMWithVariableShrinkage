dd84076d7eb757efaa690ec67d25da6e60e8b6fc
ambarket <ambarket@gmail.com>
2015-11-29 03:21:27 -0500

    Small changes from sublab

--- scripts/printIncompleteTestsForDataset.sh
+++ scripts/printIncompleteTestsForDataset.sh
@@ -1,8 +1,9 @@
 dsName=$1
-if [ $# -eq 0 ]; then
+runNum=$2
+if [ $# -ne 2 ]; then
     echo "No arguments provided"
     exit 1
 fi
 
-find "../locks/4/${dsName}" -mindepth 6 -maxdepth 6 -type d '!' -exec sh -c 'ls -1 "{}"|egrep -i -q "*doneLock.txt"' ';' -print | grep -v RevisedVariable
-find "../locks/4/${dsName}" -mindepth 7 -maxdepth 7 -type d '!' -exec sh -c 'ls -1 "{}"|egrep -i -q "*doneLock.txt"' ';' -print | grep -v Constant
+find "../locks/4/${dsName}/${runNum}" -mindepth 5 -maxdepth 5 -type d '!' -exec sh -c 'ls -1 "{}"|egrep -i -q "*doneLock.txt"' ';' -print | grep -v RevisedVariable
+find "../locks/4/${dsName}/${runNum}" -mindepth 6 -maxdepth 6 -type d '!' -exec sh -c 'ls -1 "{}"|egrep -i -q "*doneLock.txt"' ';' -print | grep -v Constant
