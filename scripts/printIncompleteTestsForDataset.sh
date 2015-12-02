5e74e51cac1c6dce6020522d56cf482a66489714
ambarket <ambarket@gmail.com>
2015-12-02 00:51:18 -0500

    scripts

--- scripts/printIncompleteTestsForDataset.sh
+++ scripts/printIncompleteTestsForDataset.sh
@@ -4,5 +4,5 @@ if [ $# -eq 0 ]; then
     exit 1
 fi
 
-find "../locks/4/${dsName}" -mindepth 6 -maxdepth 6 -type d '!' -exec sh -c 'ls -1 "{}"|egrep -i -q "*doneLock.txt"' ';' -print | grep -v RevisedVariable
-find "../locks/4/${dsName}" -mindepth 7 -maxdepth 7 -type d '!' -exec sh -c 'ls -1 "{}"|egrep -i -q "*doneLock.txt"' ';' -print | grep -v Constant
+find "../locks/${2}/${dsName}" -mindepth 6 -maxdepth 6 -type d '!' -exec sh -c 'ls -1 "{}"|egrep -i -q "*doneLock.txt"' ';' -print | grep -v RevisedVariable
+find "../locks/${2}/${dsName}" -mindepth 7 -maxdepth 7 -type d '!' -exec sh -c 'ls -1 "{}"|egrep -i -q "*doneLock.txt"' ';' -print | grep -v Constant
